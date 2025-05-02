8fb506570dd6b81306ba040bfe9c7c4c74c7f23e
nAnthonyD <99366177+nAnthonyD@users.noreply.github.com>
2025-05-02 12:58:31 +0200

    Update Combat.c

--- Combat.c
+++ Combat.c
@@ -1,1 +1,21 @@
 
+#include "character.h"
+#define CHARCOUNT 8
+
+int main(){
+    Character chars[CHARCOUNT];
+    getcharacters(chars);
+    for(int i=0;i<CHARCOUNT;i++){
+        if (chars[i].name != NULL){
+            printf("Nom: %s \n",chars[i].name);
+            printf("Special 1: %s \n",chars[i].technique1);
+            printf("Special 2: %s \n",chars[i].technique2);
+            printf("Hp: %d \n",chars[i].maxhp);
+            printf("Attack: %d \n",chars[i].attack);
+            printf("Defense: %d \n",chars[i].defense);
+            printf("Speed: %d \n",chars[i].speed);
+            printf("Agility: %d \n",chars[i].agility);
+        }
+    }
+    return 0;
+}
