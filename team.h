5863a7c4151aa38a855cb206d3c73d8279e7cfe9
bilel-z <bill.zaroui@gmail.com>
2025-05-10 23:38:08 +0200

    Update team.h

--- team.h
+++ team.h
@@ -3,18 +3,20 @@
 
 #include "character.h" 
 
-#define MAX_TEAM_SIZE 3     
+#define MAX_TEAM_SIZE 5
 #define MAX_CHARACTERS 20   
 
+
 typedef struct {
-    char team_name[50]
-    Character members[MAX_TEAM_SIZE];
+    char team_name[50];
     int current_size; 
+    Character members[MAX_TEAM_SIZE];
 } Team;
 
 
 void display_available_characters( Character all_chars[], int num_total_chars);
-void select_team_members(Team *team,  Character all_chars[], int num_total_chars,  char* team_name);
+void select_team_members(Team *team,  Character all_chars[], int num_total_chars,  char* team_name,int team_size);
 void display_team( Team *team,  char* team_name);
+void auto_select_team_members(Team *team, Character all_chars[], int num_total_chars, char* team_name_prompt,int team_size);
 
 #endif // TEAM_H
