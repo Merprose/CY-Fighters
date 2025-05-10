861f4226c4852615db6fe12d51f5980e06807e17
bilel-z <bill.zaroui@gmail.com>
2025-05-10 11:35:18 +0200

    Update ui.h

--- ui.h
+++ ui.h
@@ -8,7 +8,7 @@
 void clear_screen();
 void press_enter_to_continue();
 int display_main_menu();
-void display_combat_state(const Team* team1, const Team* team2, int p1_active_idx, int p2_active_idx, int current_player_turn); //Subject to change
+void display_combat_state( Team* team1,  Team* team2, int p1_active_idx, int p2_active_idx, int current_player_turn); //Subject to change
 void display_character_actions(); //Working On...
 void print_health_bar(int current_hp, int max_hp, int bar_width); 
 
